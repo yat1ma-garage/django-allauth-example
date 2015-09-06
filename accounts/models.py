e7477ad3a7b04bccfa2d6c541892a691273f55bc
ottatiyarou <ottatiyarou@gmail.com>
2015-09-06 14:41:52 +0900

    define custom user model

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,7 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+
+class CustomUser(AbstractUser):
+    age = models.IntegerField(verbose_name="Age")
+    github_url = models.URLField(verbose_name="GitHub URL", max_length=255)
