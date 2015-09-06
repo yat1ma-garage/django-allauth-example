61a846376b0ad0558f2f789b7efd43c0da8aff48
ottatiyarou <ottatiyarou@gmail.com>
2015-09-06 14:45:07 +0900

    added setting auth_user_model

--- sampleproj/settings.py
+++ sampleproj/settings.py
@@ -130,6 +130,7 @@ STATIC_URL = '/static/'
 
 
 # django-allauth config
+AUTH_USER_MODEL = "accounts.CustomUser"
 ACCOUNT_AUTHENTICATION_METHOD = "email"   # email
 ACCOUNT_EMAIL_REQUIRED = True             # email
 ACCOUNT_EMAIL_VERIFICATION = "mandatory"  # email
